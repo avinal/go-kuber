ec5d193b716a3ab9a207a546d2d99aa8dd9720ea
Avinal Kumar <avinal.xlvii@gmail.com>
2021-12-23 15:58:28 +0530

    add tekton
    Signed-off-by: Avinal Kumar <avinal.xlvii@gmail.com>

--- hello.go
+++ hello.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
-	"time"
 )
 
 func main() {
 	hostName, _ := os.Hostname()
-	for {
-		fmt.Printf("Hello, my hostname is %v\n", hostName)
-		time.Sleep(5 * time.Second)
-	}
+	response := fmt.Sprintf("Hello my hostname is %v\n", hostName)
+	fmt.Print(response)
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) { rw.Write([]byte(response)) })
 }
